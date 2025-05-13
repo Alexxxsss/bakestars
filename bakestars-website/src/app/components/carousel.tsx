98c97db965ff00f47e66f0646b766a5144ecc17b
Sten Kaal <sten.kaal@learnet.se>
2025-05-13 15:03:39 +0200

    carousel thing

--- bakestars-website/src/app/components/carousel.tsx
+++ bakestars-website/src/app/components/carousel.tsx
@@ -1,37 +1,45 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
 
-export function Carousel() {
+import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
+import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
+
+type imageSliderProps = {
+  imageURL: StaticImageData[];
+};
+
+export function Carousel({ imageURL }: imageSliderProps) {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  function showNextImage(): void {
+    setImageIndex((index) => (index === imageURL.length - 1 ? 0 : index + 1));
+  }
+
+  function showPrevImage(): void {
+    setImageIndex((index) => (index === 0 ? imageURL.length - 1 : index - 1));
+  }
+
   return (
-    <div className="flex flex-row gap-4 w-full justify-center items-center mb-100">        
-        <button className="rounded-full bg-compliment w-12 flex items-center justify-center font-bold text-xl hover:scale-120 hover:text-stone-600 transition-transform-colors duration-300">&#60;</button>
-            <Link href={"/"} className="shadow-lg shadow-black rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300">
-                <Image
-                className="blur-sm"
-                src="/placeholder.jpg"
-                alt="Placeholder Image"
-                width={720}
-                height={480}
-                priority/>
-            </Link>
-            <Link href={"/"} className="shadow-lg shadow-black rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300 z-2" style={{ marginLeft: '-200px', marginRight: '-200px' }}>
-                <Image
-                src="/Cannjohn.png"
-                alt="Placeholder Image"
-                width={1440}
-                height={960}
-                priority/>
-            </Link>
-            <Link href={"/"} className="shadow-lg shadow-black rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300">
-                <Image
-                className="blur-sm"
-                src="/Image_Sequence.png"
-                alt="Placeholder Image"
-                width={720}
-                height={480}
-                priority/>
-            </Link>
-        <button className="rounded-full bg-compliment w-12 flex items-center justify-center font-bold text-xl hover:scale-120 hover:text-stone-600 transition-transform-colors duration-300">&#62;</button>
+    <div className="flex flex-row">
+      <button onClick={showPrevImage} className="top-1/2 left-0 z-10">
+        <ArrowBigLeft />
+      </button>
+      <div className="w-full h-[500px] relative overflow-hidden">
+        <div className="w-full h-full flex">
+          {imageURL.map((url) => (
+            <Image
+              key={url.src}
+              alt="Carousel Picture"
+              src={url}
+              className="object-cover w-full h-[500px] block flex-shrink-0 flex-grow-0 transition-transform duration-500 ease-in-out"
+              style={{ transform: `translateX(-${imageIndex * 100}%)` }}
+            />
+          ))}
+        </div>
+      </div>
+      <button onClick={showNextImage} className="top-1/2 right-0 z-10">
+        <ArrowBigRight />
+      </button>
     </div>
   );
 }
--- bakestars-website/src/app/page.tsx
+++ bakestars-website/src/app/page.tsx
@@ -2,17 +2,28 @@ import Image from "next/image";
 import Navbar from "./components/navbar";
 import { Carousel } from "./components/carousel";
 import { Contact } from "./components/contact";
- 
+import logo from "./images/logo.png";
+
+import image1 from "./images/BlueberryPieGuy.png";
+import image2 from "./images/Cannjohn.png";
+import image3 from "./images/Image_Sequence.png";
+import image4 from "./images/SnowGobst.png";
+import image5 from "./images/Hones.png";
+
+const IMAGES = [image1, image2, image3, image4, image5];
+
 export default function Home() {
   return (
-                
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
       <Navbar />
       <main className=" flex flex-col p-10 gap-8 items-center justify-center">
         <h1 className="font-bold text-4xl ">Featured Games</h1>
-        <Carousel />
+        <div className=" max-w-[1300px] max-h-[500px] h-full m-0-auto">
+          <Carousel imageURL={IMAGES} />
+        </div>
+
         <Image
-          src="/logo.png"
+          src={logo}
           alt="Bake Stars Studios Logo"
           className="invert"
           width={360}
@@ -31,7 +42,9 @@ export default function Home() {
           <Contact />
         </div>
       </main>
-      <footer className="grid py-8 text-center text-sm text-gray-500 grid-cols-4 items-start-4 justify-around">© 2025 Bake Stars Studios</footer>
+      <footer className="grid py-8 text-center text-sm text-gray-500 grid-cols-4 items-start-4 justify-around">
+        © 2025 Bake Stars Studios
+      </footer>
     </div>
   );
 }
